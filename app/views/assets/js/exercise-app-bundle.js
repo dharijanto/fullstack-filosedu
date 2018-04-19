2c588861058edb152bd261dfd68cde4756961d6d
Denny Harijanto <denny@nusantara-cloud.com>
2018-04-19 15:32:43 +0700

    Updated Bundled JS

--- app/views/assets/js/exercise-app-bundle.js
+++ app/views/assets/js/exercise-app-bundle.js
@@ -217,9 +217,7 @@ function postAnswer() {
 $('#resetQuestion').on('click', function (e) {
   postAnswer().then(function (resp) {
     if (resp.status) {
-      setTimeout(function () {
-        window.location.reload();
-      }, 2000);
+      window.location.reload();
     } else {
       window.location.reload();
     }
