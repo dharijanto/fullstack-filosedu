76af5d3fd7e7e6ba0e7e03937bb643949c6986b5
Denny <denny@nusantara-cloud.com>
2018-12-24 17:25:11 +0700

    Updated Bundled Assets

---
+++ dist/lib/utils/formatter-neo.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJsaWIvdXRpbHMvZm9ybWF0dGVyLW5lby5qcyIsInNvdXJjZXNDb250ZW50IjpbXX0=
